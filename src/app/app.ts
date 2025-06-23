a69b5253684671c5f62fa12d7be304e8c3bbe655
NeoJhonn <jhonny.azevedo@gmail.com>
2025-06-22 22:14:47 -0300

    Card products created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { MatCardModule } from '@angular/material/card';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     Header,
+    MatCardModule,
+    MatButtonModule,
   ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
