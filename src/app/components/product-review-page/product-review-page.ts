58868af279f9d9afbc0a7c0813eb80ed2c55fc46
NeoJhonn <jhonny.azevedo@gmail.com>
2025-06-29 20:11:35 -0300

    Added metadata SEO for products

--- src/app/components/product-review-page/product-review-page.ts
+++ src/app/components/product-review-page/product-review-page.ts
@@ -4,16 +4,14 @@ import { HttpClient } from '@angular/common/http';
 import { ProductReview } from '../product-review/product-review';
 import { MaterialModule } from '../../material.module';
 import { ChangeDetectorRef } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-product-review-page',
   standalone: true,
-  imports: [
-    ProductReview,
-    MaterialModule
-  ],
+  imports: [ProductReview, MaterialModule],
   templateUrl: './product-review-page.html',
-  styleUrl: './product-review-page.scss'
+  styleUrl: './product-review-page.scss',
 })
 export class ProductReviewPage implements OnInit {
   private route = inject(ActivatedRoute);
@@ -22,12 +20,14 @@ export class ProductReviewPage implements OnInit {
 
   product?: any;
 
+  constructor(private title: Title, private meta: Meta) {}
+
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       const slug = params.get('slug');
       if (slug) {
         this.loadProduct(slug);
-        console.log("slud da rota: ",slug);
+        console.log('slud da rota: ', slug);
       }
     });
   }
@@ -36,6 +36,29 @@ export class ProductReviewPage implements OnInit {
     this.http.get<any[]>('/assets/data/products.json').subscribe(products => {
       const found = products.find(p => p.slug === slug);
       this.product = found;
+      this.title.setTitle(`${this.product.productTitle} | ClickReviews`);
+
+      this.meta.updateTag({
+        name: 'description',
+        content: this.product.opinion,
+      });
+
+      this.meta.updateTag({
+        property: 'og:title',
+        content: this.product.productTitle,
+      });
+      this.meta.updateTag({
+        property: 'og:description',
+        content: this.product.opinion,
+      });
+      this.meta.updateTag({
+        property: 'og:image',
+        content: this.product.imageUrl,
+      });
+      this.meta.updateTag({
+        property: 'og:url',
+        content: window.location.href,
+      });
       this.cdr.detectChanges(); // <- força atualização de estado para carregar a página
       console.log('Produto carregado:', found);
     });
