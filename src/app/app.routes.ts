19513a0d193a1eeaa6a84099c06be1432b15a56e
NeoJhonn <jhonny.azevedo@gmail.com>
2025-07-06 21:04:15 -0300

    Add a resolver in Home route for meta tags

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home.component/home.component';
 import { ProductReviewPageComponent } from './components/product-review-page.component/product-review-page.component';
 import { ReachOutComponent } from './components/reach-out.component/reach-out.component';
+import { HomeMetaResolver } from './components/home.component/home-meta.resolver';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent,
+    resolve: { meta: HomeMetaResolver } },
   { path: 'review/:slug', component: ProductReviewPageComponent },
   { path: 'contato', component: ReachOutComponent }
 ];
--- src/app/components/home.component/home.component.ts
+++ src/app/components/home.component/home.component.ts
@@ -15,38 +15,5 @@ import { isPlatformBrowser } from '@angular/common';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  constructor(@Inject(PLATFORM_ID) private platformId: Object,
-  private title: Title, private meta: Meta) {
 
-   }
-
-   ngOnInit(): void {
-    // SEO Metadata
-    this.title.setTitle(`Home | ClickReviews`);
-
-    // Limpa tags anteriores (evita duplicação)
-    this.meta.removeTag("name='description'");
-    this.meta.removeTag("property='og:title'");
-    this.meta.removeTag("property='og:description'");
-    this.meta.removeTag("property='og:image'");
-    this.meta.removeTag("property='og:url'");
-    this.meta.removeTag("property='og:type'");
-    this.meta.removeTag("name='twitter:card'");
-    this.meta.removeTag("name='twitter:title'");
-    this.meta.removeTag("name='twitter:description'");
-    this.meta.removeTag("name='twitter:image'");
-
-
-    this.meta.updateTag({
-        property: 'og:description',
-        content: 'ClickReviews, o melhor site de Reviews do Brasil!',
-      });
-    if (isPlatformBrowser(this.platformId)) {
-      let url = window.location.href;
-      this.meta.updateTag({
-      property: 'og:url',
-      content: url,
-    });
-    }
-   }
 }
