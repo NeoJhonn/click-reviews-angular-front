99be6ba38ccf5df28cf9e349cf69c0802fa927c2
Jhonny Azevedo <jhonny.azevedo@gmail.com>
2025-07-07 15:38:55 -0300

    Delete api directory

--- api/index.mjs
+++
@@ -1,8 +0,0 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const { createServer } = require('http');
-const { handler } = require('../dist/click-reviews-angular-front/server/server.mjs');
-
-export default async function(req, res) {
-  return handler(req, res);
-}
