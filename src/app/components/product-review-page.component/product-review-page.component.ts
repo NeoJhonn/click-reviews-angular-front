ada1bcb41b536658d4a83f927291fbda698b39cb
NeoJhonn <jhonny.azevedo@gmail.com>
2025-07-07 00:58:01 -0300

    Added isPlatformBrowser in ProductReviewPageComponent

--- src/app/components/product-review-page.component/product-review-page.component.ts
+++ src/app/components/product-review-page.component/product-review-page.component.ts
@@ -1,11 +1,10 @@
-import { Component, Inject, inject, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, inject } from '@angular/core';
 import { ProductReviewComponent } from '../product-review.component/product-review.component';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { ChangeDetectorRef } from '@angular/core';
+import { ChangeDetectorRef, isPlatformBrowser } from '@angular/core';
 import { Title, Meta } from '@angular/platform-browser';
 import { MaterialModule } from '../../material.module-module';
-
 
 @Component({
   selector: 'app-product-review-page',
@@ -22,21 +21,26 @@ export class ProductReviewPageComponent {
   private cdr = inject(ChangeDetectorRef);
 
   product?: any;
+  isBrowser: boolean;
 
   constructor(
     private title: Title,
     private meta: Meta,
     @Inject(PLATFORM_ID) private platformId: Object
-  ) {}
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      const slug = params.get('slug');
-      if (slug) {
-        this.loadProduct(slug);
-        console.log('slug da rota: ', slug);
-      }
-    });
+    if (this.isBrowser) {
+      this.route.paramMap.subscribe(params => {
+        const slug = params.get('slug');
+        if (slug) {
+          this.loadProduct(slug);
+          console.log('slug da rota: ', slug);
+        }
+      });
+    }
   }
 
   loadProduct(slug: string): void {
@@ -45,39 +49,38 @@ export class ProductReviewPageComponent {
       this.product = found;
 
       // Atualiza o título da aba
-    this.title.setTitle(`${this.product.productTitle} | ClickReviews`);
+      this.title.setTitle(`${this.product.productTitle} | ClickReviews`);
 
-    // Limpa tags anteriores (evita duplicação)
-    this.meta.removeTag("name='description'");
-    this.meta.removeTag("property='og:title'");
-    this.meta.removeTag("property='og:description'");
-    this.meta.removeTag("property='og:image'");
-    this.meta.removeTag("property='og:url'");
-    this.meta.removeTag("property='og:type'");
-    this.meta.removeTag("name='twitter:card'");
-    this.meta.removeTag("name='twitter:title'");
-    this.meta.removeTag("name='twitter:description'");
-    this.meta.removeTag("name='twitter:image'");
+      // Limpa tags anteriores
+      this.meta.removeTag("name='description'");
+      this.meta.removeTag("property='og:title'");
+      this.meta.removeTag("property='og:description'");
+      this.meta.removeTag("property='og:image'");
+      this.meta.removeTag("property='og:url'");
+      this.meta.removeTag("property='og:type'");
+      this.meta.removeTag("name='twitter:card'");
+      this.meta.removeTag("name='twitter:title'");
+      this.meta.removeTag("name='twitter:description'");
+      this.meta.removeTag("name='twitter:image'");
 
+      // Open Graph
+      this.meta.addTags([
+        { property: 'og:title', content: this.product.productTitle },
+        { property: 'og:description', content: this.product.opinion },
+        { property: 'og:image', content: this.product.imageUrl },
+        { property: 'og:url', content: `https://clickreviews.com.br/review/${this.product.slug}` },
+        { property: 'og:type', content: 'website' },
+      ]);
 
-    // Open Graph
-    this.meta.addTags([
-      { property: 'og:title', content: this.product.productTitle },
-      { property: 'og:description', content: this.product.opinion },
-      { property: 'og:image', content: this.product.imageUrl },
-      { property: 'og:url', content: `https://clickreviews.com.br/review/${this.product.slug}` },
-      { property: 'og:type', content: 'website' },
-    ]);
+      // Twitter Card
+      this.meta.addTags([
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: this.product.productTitle },
+        { name: 'twitter:description', content: this.product.opinion },
+        { name: 'twitter:image', content: this.product.imageUrl },
+      ]);
 
-    // Twitter Card
-    this.meta.addTags([
-      { name: 'twitter:card', content: 'summary_large_image' },
-      { name: 'twitter:title', content: this.product.productTitle },
-      { name: 'twitter:description', content: this.product.opinion },
-      { name: 'twitter:image', content: this.product.imageUrl },
-    ]);
-
-      this.cdr.detectChanges(); // <- força atualização de estado para carregar a página
+      this.cdr.detectChanges();
       console.log('Produto carregado:', found);
     });
   }
