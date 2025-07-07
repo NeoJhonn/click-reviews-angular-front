9cedbcbccd286aefb30f95a46bb1f6d12ddf9c12
NeoJhonn <jhonny.azevedo@gmail.com>
2025-07-07 02:26:06 -0300

    Added Univesal SSR routing fix10

--- src/server.ts
+++ src/server.ts
@@ -66,3 +66,6 @@ if (isMainModule(import.meta.url)) {
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
  */
 export const reqHandler = createNodeRequestHandler(app);
+
+export const handler = createNodeRequestHandler(app);
+
