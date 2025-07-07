4fb99e345f2a34cc9bb2e2773d80d678df8614c6
NeoJhonn <jhonny.azevedo@gmail.com>
2025-07-07 02:18:34 -0300

    Added Univesal SSR routing fix8

--- api/index.mjs
+++ api/index.mjs
@@ -1,5 +1,8 @@
-import { app } from '../dist/click-reviews-angular-front/server/main.server.mjs';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const { createServer } = require('http');
+const { handler } = require('../dist/click-reviews-angular-front/server/server.mjs');
 
-export default async function handler(req, res) {
-  app(req, res);
+export default async function(req, res) {
+  return handler(req, res);
 }
