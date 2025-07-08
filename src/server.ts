a3a3926aea63b14bfa836c50a84cf612091f9ae9
Jhonny Azevedo <jhonny.azevedo@gmail.com>
2025-07-08 19:27:33 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import express from 'express';
 import { join } from 'node:path';
 import fs from 'fs/promises';
 
-const browserDistFolder = join('indexFile.html', '../browser/indexFile.html');
+const browserDistFolder = join('indexFile', '../browser');
 export const app = express();
 const angularApp = new AngularNodeAppEngine();
 
