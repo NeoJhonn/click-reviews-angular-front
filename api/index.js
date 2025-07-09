7e39599967861947d777a606b8e7bbfc9cba6f27
NeoJhonn <jhonny.azevedo@gmail.com>
2025-07-09 01:06:07 -0300

    SSR finally online6

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 // api/index.js
-import { createRequestHandler } from '@angular/ssr/vercel';
+//import { createRequestHandler } from '@angular/ssr/vercel';
 import { app } from '../dist/click-reviews-angular-front/server/server.mjs';
 
 export default createRequestHandler(app);
