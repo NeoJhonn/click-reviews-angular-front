37b9a6eeeff2a7b9204ae54f7bb4e053ee11d1e3
NeoJhonn <jhonny.azevedo@gmail.com>
2025-07-10 00:53:04 -0300

    All metatags set from server side3

--- src/server.ts
+++ src/server.ts
@@ -71,8 +71,8 @@ app.use(async (req, res, next) => {
     let html = await streamToString(response.body);
 
     // Se for rota Home
-    console.log('o que tem aqui', req.url?.split('favicon.ico')[0]?.trim());
-    if (req.url === '/') {
+    console.log('o que tem aqui', req.url?.split('favicon.ico')[0].trim());
+    if (req.url === '/api/index') {
       console.log('Entrou na rota do Home');
       const titleTag = `<title>Home | ClickReviews</title>`;
       const metaTags = `
