25dd99d49d4626d04a11b0158eaad6e024da67bd
NeoJhonn <jhonny.azevedo@gmail.com>
2025-07-10 01:16:48 -0300

    All metatags set from server side6

--- src/server.ts
+++ src/server.ts
@@ -71,11 +71,10 @@ app.use(async (req, res, next) => {
     let html = await streamToString(response.body);
 
     // Se for rota Home
-    const pathname = req.url?.split('favicon.ico')[0]?.trim();
-    const pathname2 = req.url?.split('favicon.png')[0]?.trim();
-    console.log('o que tem aqui: ', pathname +"  "+ pathname2);
-
-    if (pathname === '/' || pathname2 === '/') {
+    const pathname = req.url?.split('?')[0]?.trim();
+    console.log('o que tem aqui: ', pathname);
+    if ( pathname === '/' || pathname === '' || pathname === '/index.html'
+) {
       console.log('Entrou na rota do Home');
       const titleTag = `<title>Home | ClickReviews</title>`;
       const metaTags = `
