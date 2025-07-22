6fb502dcd929afd8eb5f6e548e36cc9b81b8c202
Jhonny Azevedo <jhonny.azevedo@gmail.com>
2025-07-22 19:21:10 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,4 @@
-export default async function handler(req, res) {
-  // Redireciona de www.clickreviews.com.br para clickreviews.com.br
-  const host = req.headers.host;
-  if (host && host.startsWith('www.')) {
-    const redirectTo = `https://${host.replace(/^www\./, '')}${req.url}`;
-    res.writeHead(301, {
-      Location: redirectTo
-    });
-    res.end();
-    return;
-  }
-
-  // Continua com o SSR normalmente
+// Inicia servidor SSR
   const { handler } = await import('../dist/click-reviews-angular-front/server/server.mjs');
   return handler(req, res);
 }
