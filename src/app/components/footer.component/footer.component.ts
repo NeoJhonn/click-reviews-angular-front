bb23a87408989ebc3509578407b2f9a41bb70e6a
NeoJhonn <jhonny.azevedo@gmail.com>
2025-07-24 01:03:56 -0300

    Added Google Ad in footer

--- src/app/components/footer.component/footer.component.ts
+++ src/app/components/footer.component/footer.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, AfterViewInit } from '@angular/core';
 import { MaterialModule } from '../../material.module-module';
+import { isPlatformBrowser } from '@angular/common';
+
 
 @Component({
   selector: 'app-footer',
@@ -10,6 +12,19 @@ import { MaterialModule } from '../../material.module-module';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      try {
+        ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({});
+      } catch (e) {
+        console.error('Erro ao carregar anúncio do AdSense:', e);
+      }
+    }
+  }
+
   currentYear: number = new Date().getFullYear();
   socialLinks = [
     { icon: 'assets/icons/instagram.svg', url: 'https://www.instagram.com/click__reviews/' }
